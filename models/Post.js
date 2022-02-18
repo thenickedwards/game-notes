7fd2af7e70ba59ff7a23230bbf61626876b17d2d
Nick Edwards <theadventuresofnickedwards@gmail.com>
2022-02-17 21:55:17 -0800

    Database seededgit add .

--- models/Post.js
+++ models/Post.js
@@ -37,7 +37,7 @@ Post.init(
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'comment',
+    modelName: 'post',
   }
 );
 
--- models/User.js
+++ models/User.js
@@ -8,7 +8,6 @@ class User extends Model {
   }
 }
 
-// TODO: Update key-value pairs here
 User.init(
   {
     id: {
