186b10ce3cd04160dbd4038cef1e3f2b456dcdc1
Nick Edwards <theadventuresofnickedwards@gmail.com>
2022-02-18 18:44:25 -0800

    Troubelshooting server

---
+++ controllers/api/postRoutes.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+const { Post, User } = require('../../models');
+
+router.get('/', async (req, res) => {
+    try {
+        const postsData = await Post.findAll({
+            include: [{ model: User }],
+        });
+        res.status(200).json(postsData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
