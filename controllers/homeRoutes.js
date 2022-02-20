cb2e45df5ad4881dcc739444ed5c657df50c89c6
Nick Edwards <theadventuresofnickedwards@gmail.com>
2022-02-19 16:48:55 -0800

    Troubleshooting homepage render in homeRoutes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,24 +2,25 @@ const router = require('express').Router();
 const { User, Post } = require('../models');
 // const withAuth = require('../utils/auth');
 
-router.get('./', async (req, res) => {
+router.get('/', async (req, res) => {
   try {
-    // const postsData = await Post.findAll({
-    //   include: [
-    //     {
-    //       model: User,
-    //       attributes: ['username']
-    //     },
-    //   ],
-    // });
+    const postsData = await Post.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['username']
+        },
+      ],
+    });
     
-    // const allPosts = postsData.map((post) => post.get({ plain: true }));
+    const allPosts = postsData.map((post) => post.get({ plain: true }));
 
     res.render('homepage', {
-      // allPosts,
+      allPosts,
       // logged_in: req.session.logged_in
     })
   } catch (err) {
+    console.log(err);
     res.status(500).json(err);
   }
 });
