f5239169f77633bb5b730209489d8678e33c8a4f
Nick Edwards <theadventuresofnickedwards@gmail.com>
2022-02-19 18:45:27 -0800

    Troubleshooting get one

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const { User, Post } = require('../models');
 // const withAuth = require('../utils/auth');
 
+// GET all posts for homepage/dashboard
 router.get('/', async (req, res) => {
   try {
     const postsData = await Post.findAll({
@@ -24,5 +25,28 @@ router.get('/', async (req, res) => {
     res.status(500).json(err);
   }
 });
+
+// GET a post
+router.get('/posts/:id', async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id, {
+      include: [{ model: User, attributes: ['username'] }]
+    });
+
+    if (!postData) {
+      res.status(404).json({ message: 'Sorry, we cant find this post.'});
+      return;
+    }
+
+    const onePost = postData.get({ plain: true });
+
+    res.render('post', {onePost})
+    res.status(200),json(postData);
+  } catch {
+    // console.log(err);
+    res.status(500).json(err);
+  }
+});
+
 
 module.exports = router;
